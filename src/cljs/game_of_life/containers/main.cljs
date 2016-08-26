d0919e595ca46ba23be21bfca1832291508b1481
Alan R. Soares <alanrsoars@gmail.com>
2016-08-26 17:31:49 +1200

    setInterval -> requestAnimationFrame

--- src/cljs/game_of_life/containers/main.cljs
+++ src/cljs/game_of_life/containers/main.cljs
@@ -2,10 +2,12 @@
     (:require [game-of-life.components.grid :refer [grid-component]]
               [re-frame.core :refer [subscribe
                                      dispatch]]))
-
-(defonce tick 
-  (js/setInterval
-    #(dispatch [:next-gen]) 100))
+((defn tick []
+  (js/requestAnimationFrame
+    (fn []
+      (do
+        (dispatch [:next-gen])
+        (tick))))))       
 
 (defn main-container []
   (let [board (subscribe [:board])]
