0e3966a9085af8a7139b0a39f4e22bf46f168b33
Alan R. Soares <alanrsoars@gmail.com>
2016-08-29 12:59:41 +1200

    cleanup subscriptions even more

--- src/cljs/game_of_life/subs.cljs
+++ src/cljs/game_of_life/subs.cljs
@@ -1,11 +1,10 @@
 (ns game-of-life.subs
-  (:require-macros [reagent.ratom :refer [reaction]])
   (:require [re-frame.core :refer [reg-sub]]))
 
 (reg-sub
- :board
- #(:board %))
+  :board
+  :board)
 
 (reg-sub
   :profiler
-  #(:profiler %))
+  :profiler)
